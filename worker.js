e7dd131d956960348a2d99f34a58a29c66a4ebb2
KuwiNet <darnycn@gmail.com>
2025-04-19 17:30:20 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -3,7 +3,7 @@
 /**
  * static files (404.html, sw.js, conf.js)
  */
-const ASSET_URL = 'https://kuwinet.github.io/git-proxy/'
+const ASSET_URL = 'https://kuwinet.github.io/CF-Ghproxy/'
 // 前缀，如果自定义路由为example.com/gh/*，将PREFIX改为 '/gh/'，注意，少一个杠都会出错！
 const PREFIX = '/'
 // 分支文件使用jsDelivr镜像的开关，0为关闭，默认关闭
